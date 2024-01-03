994cf31e609bee82d98dab1650df242bf368f4d0
KIMHEEJIN <hee031812@gmail.com>
2024-01-03 11:17:20 +0900

    git setting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react'
+import useMouse from './util/useMouse'
+import { motion } from 'framer-motion'
 
-function App() {
+
+const App = () => {
+  const [isHovered, setIsHovered] = useState(false);
+  const section2Ref = useRef(null);
+  const size = isHovered ? 500 : 50;
+  const { x, y } = useMouse(section2Ref);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <section id='section01'>
+        <h1>Gallery Hover Animation</h1>
+      </section>
+
+      <section id='section02' ref={section2Ref}>
+        <div className='project__wrap'>
+          <motion.div
+            className='project__mask'
+            animate={{
+              WebkitMaskPosition: `${x - size / 2}px ${y - size / 2}px`,
+              WebkitMaskSize: `${size}px`,
+            }}
+            transition={{ type: "tween", ease: "backOut", duration: 0.5 }}
+          >
+            <p onMouseEnter={() => { setIsHovered(true) }}
+              onMouseLeave={() => { setIsHovered(false) }}>"어려움은 극복해 나가는 것이다."<br />라는 말을 항상 <span>기억합니다.</span> </p>
+          </motion.div>
+          <div className='project__text'>
+            <p>I'm heejin kim, I always<span> remember</span> saying, "The difficulty is to overcome."</p>
+          </div>
+        </div>
+      </section>
+
+      <section id='section03'></section>
+    </main>
+  )
 }
 
-export default App;
+export default App
---
+++ src/util/useMouse.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+const useMouse = (ref) => {
+    const [mousePosition, setMousePosition] = useState({ x: null, y: null });
+
+
+
+    const updateMouse = e => {
+        if (ref.current) {
+            const { left, top } = ref.current.getBoundingClientRect();
+            const x = e.clientX - left;
+            const y = e.clientY - top;
+            setMousePosition({ x, y });
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("mousemove", updateMouse);
+
+        return () => window.removeEventListener("mousemove", updateMouse);
+    }, [ref]);
+
+    return mousePosition;
+}
+
+export default useMouse;
